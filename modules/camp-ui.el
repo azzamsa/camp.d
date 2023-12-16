f7157f44184e787b701699c62c2e8d850716138b
azzamsa <me@azzamsa.com>
2023-12-16 07:57:52 +0700

    feat: insert daily quote

--- modules/camp-ui.el
+++ modules/camp-ui.el
@@ -80,6 +80,18 @@ Invoke again to revert to the window configuration before it was activated."
   :straight t
   :after evil evil-collection
   :demand t
+  :init
+  (defun aza/dashboard-insert-quote (list-size)
+    "Inserts a random non-comment quote from the 'quotes' file into the dashboard."
+    (dashboard-insert-heading "Quote of the Day:" nil (all-the-icons-faicon "commenting-o"))
+    (insert "\n")
+    (let* ((lines (with-temp-buffer
+                    (insert-file-contents (concat camp-etc-dir "quotes"))
+                    (split-string (buffer-string) "\n" t)))
+           (filtered-lines (cl-remove-if (lambda (line) (string-match-p "^\\s-*#" line)) lines))
+           (random-line (when filtered-lines
+                          (string-join (split-string (nth (random (length filtered-lines)) filtered-lines)) " "))))
+      (insert "    " random-line)))
   :custom
   (dashboard-set-heading-icons t)
   (dashboard-set-file-icons t)
@@ -90,6 +102,14 @@ Invoke again to revert to the window configuration before it was activated."
   (dashboard-image-banner-max-width 600)
   (dashboard-projects-backend 'project-el)
   (dashboard-startup-banner (concat user-emacs-directory "docs/logo.png"))
+  (dashboard-items '((daily-quote)
+                     (recents . 5)
+                     (projects . 5)
+                     (bookmarks . 5)))
+  (dashboard-item-generators '((daily-quote . aza/dashboard-insert-quote)
+                               (recents . dashboard-insert-recents)
+                               (projects . dashboard-insert-projects)
+                               (bookmarks . dashboard-insert-bookmarks)))
   :config
   ;; Ensure setting the keybindings before openning the dashboard
   (evil-collection-dashboard-setup)
