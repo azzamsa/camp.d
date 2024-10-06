6bc24bd6402398cf0e48208b0b7758e4b4625925
Azzam S.A <noreply@azzamsa.com>
2024-10-06 12:49:59 +0700

    feat: use `lsp-bridge`

--- modules/camp-prog.el
+++ modules/camp-prog.el
@@ -45,6 +45,13 @@
         ;; redundant with K
         lsp-ui-doc-enable nil))
 
+(use-package lsp-bridge
+  :straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
+            :files (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
+            :build (:not compile))
+  :init
+  (global-lsp-bridge-mode))
+
 (use-package editorconfig :straight t)
 
 (use-package rainbow-mode
