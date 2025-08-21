0460f56b749e31e052b6525609bda397d1f89eed
Azzam S.A <noreply@azzamsa.com>
2025-08-22 03:26:53 +0700

    feat: make `quotes` cli optional
    Ease my distro hopping.

--- modules/camp-ui.el
+++ modules/camp-ui.el
@@ -142,7 +142,7 @@ Invoke again to revert to the window configuration before it was activated."
     "Insert a random quote into the dashboard."
     (dashboard-insert-heading "Quote of the Day:" nil (nerd-icons-faicon "nf-fa-commenting_o" :face 'dashboard-heading))
     (insert "\n")
-    (insert "    " (propertize (string-trim (shell-command-to-string "quotes --period --quoted --emoji")) 'face 'bold) "\n"))
+    (insert "    " (propertize (string-trim (if (executable-find "quotes") (shell-command-to-string "quotes --period --quoted --emoji") "🦾 Practice, It's Practice, Practice.")) 'face 'bold) "\n"))
   :custom
   (dashboard-set-heading-icons t)
   (dashboard-set-file-icons t)
