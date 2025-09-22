ce2683d40a4abf21591456ae11e568783a79f4a1
Azzam S.A <noreply@azzamsa.com>
2025-09-23 04:07:48 +0700

    fix: add `private-dir` to yasnippet

--- modules/camp-snippets.el
+++ modules/camp-snippets.el
@@ -3,21 +3,12 @@
 ;; A template system for Emacs
 (use-package yasnippet
   :ensure t
-  :init
-  ;; Reduce default verbosity. 3 is too chatty about initializing yasnippet. 2
-  ;; is just right (only shows errors).
-  (defvar yas-verbosity 2)
-  ;; Remove default ~/.emacs.d/snippets
-  (defvar yas-snippet-dirs nil)
   :config
-  ;; ~/.emacs.d/etc/yasnippet/snippets
-  (setq private-yas-dir (expand-file-name "yasnippet/snippets/" camp-etc-dir))
-  (+ensure-directory private-yas-dir)
+  (setq private-yas-dir (no-littering-expand-etc-file-name "yasnippet/snippets"))
   (push private-yas-dir yas-snippet-dirs)
 
   (yas-reload-all)
   (yas-global-mode +1))
-
 
 ;; A collection of yasnippet snippets for many languages
 (use-package yasnippet-snippets
@@ -25,7 +16,8 @@
   :after yasnippet)
 
 ;; The Doom Emacs snippets library
-(use-package doom-snippets
-  :ensure (:host github :repo "hlissner/doom-snippets" :files ("*.el" "*")))
+;; (use-package doom-snippets
+;;   :ensure (:host github :repo "hlissner/doom-snippets" :files ("*.el" "*"))
+;;   :after yasnippet)
 
 (provide 'camp-snippets)
