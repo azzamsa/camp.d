965114be3e80b547d4aa9f0e76a87d080d676753
Azzam S.A <noreply@azzamsa.com>
2026-01-11 12:07:36 +0700

    style: less opinionated `modus vivindi tinted` theme

--- modules/camp-theme.el
+++ modules/camp-theme.el
@@ -6,43 +6,22 @@
   (setq modus-themes-italic-constructs t
         modus-themes-bold-constructs t)
 
-  ;; Modeline & Line-number will be the same across themes.
-  ;; Other faces will be leave as is.
   (setq modus-themes-common-palette-overrides
         '(
-          (black      "#000000")
-          ;; Catppuccin Mocha palette
-          (text       "#cdd6f4")
-          (subtext1   "#bac2de")
-          (subtext0   "#a6adc8")
-          (overlay2   "#9399b2")
-          (overlay0   "#7f849c")
-          (surface0   "#313244")
-          (base       "#1e1e2e")
-          (crust      "#11111b")
+          (bg-main     "#000000")
+          (fg-dim      "#989898")
 
           ;; All the colors below are kept across theme.
-          ;; Mostly taken from https://catppuccin.com/palette mocha.
-          (bg-main   black)
+          (bg-main   bg-main)
           ;; Modus Vivendi Tinted uses `red-faint` instead of `fg-dim` as a comment.
-          (comment subtext0)
-          (bg-region surface0)
+          (comment fg-dim)
+          (bg-region bg-dim)
 
           ;; Modeline
-          ;; (fg-mode-line-active text)
-          (bg-mode-line-active base)
+          (bg-mode-line-active bg-dim)
           (border-mode-line-active bg-mode-line-active)
-          ;; (fg-mode-line-inactive overlay2)
-          (bg-mode-line-inactive crust)
-          (border-mode-line-inactive bg-mode-line-inactive)
 
           ;; Line numbers
-          ;; (fg-line-number-active text)
-          (bg-line-number-active surface0)
-          ;; (fg-line-number-inactive overlay2)
-          (bg-line-number-inactive crust)
-
-          ;; Fringe
-          (fringe crust)))
+          (bg-line-number-active bg-dim)))
 
   (load-theme 'modus-vivendi-tinted t))
