ca98c732b358ccfd7857b0ab0bb8952f19f7b84e
Azzam S.A <noreply@azzamsa.com>
2026-01-19 12:44:58 +0700

    style: simpler modus vivendi theme

--- modules/camp-theme.el
+++ modules/camp-theme.el
@@ -9,17 +9,21 @@
   (setq modus-themes-common-palette-overrides
         '(
           (bg-main     "#000000")
+          (bg-active   "#1d2235") ; bg-dim
+          (bg-dim      "#0d0e1c") ; bg-main
           (fg-dim      "#989898")
 
           ;; All the colors below are kept across theme.
           (bg-main   bg-main)
           ;; Modus Vivendi Tinted uses `red-faint` instead of `fg-dim` as a comment.
           (comment fg-dim)
-          (bg-region bg-dim)
+          (bg-region bg-active)
 
           ;; Modeline
-          (bg-mode-line-active bg-dim)
+          (bg-mode-line-active bg-active)
           (border-mode-line-active bg-mode-line-active)
+          (bg-mode-line-inactive bg-dim)
+          (border-mode-line-inactive bg-mode-line-inactive)
 
           ;; Line numbers
           (bg-line-number-active bg-dim)))
