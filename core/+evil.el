28b3315cc997b47d2f1a6fbbada7dbb233d69ca3
Azzam S.A <noreply@azzamsa.com>
2026-03-11 16:05:41 +0700

    feat: revert multi-cursors support

--- core/+evil.el
+++ core/+evil.el
@@ -109,4 +109,20 @@
   :init
   (evil-goggles-mode))
 
+;; Multiple cursors for evil-mode, based on iedit
+(use-package evil-multiedit
+  :ensure t
+  :after evil
+  :init
+  (+nvmap!
+    "R"      '(evil-multiedit-match-all              :wk "Match all occurrences")
+    "M-d"    '(evil-multiedit-match-symbol-and-next  :wk "Match symbol and go to next")
+    "M-D"    '(evil-multiedit-match-symbol-and-prev  :wk "Match symbol and go to previous")
+    "M-d"    '(evil-multiedit-match-and-next         :wk "Match and go to next")
+    "M-D"    '(evil-multiedit-match-and-prev         :wk "Match and go to previous")
+    "C-M-d"  '(evil-multiedit-restore                :wk "Restore previous match"))
+  :config
+  (evil-multiedit-default-keybinds))
+
+
 (provide '+evil)
