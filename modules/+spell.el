b8b205aaddd514c23081cc9a9ca8bac5dbe4b622
Azzam S.A <noreply@azzamsa.com>
2026-04-22 08:40:21 +0700

    feat: enable `jinx` globally

--- modules/+spell.el
+++ modules/+spell.el
@@ -3,8 +3,13 @@
 ;; Just-in-time spell checker
 (use-package jinx
   :ensure t
+  :hook (emacs-startup . global-jinx-mode)
+  :bind ([remap ispell-word] . jinx-correct)
   :config
- (dolist (hook '(text-mode-hook prog-mode-hook))
-    (add-hook hook #'jinx-mode)))
+  (setq jinx-languages "en_US id_ID"))
+
+;; Distraction-free words correction with `flyspell' via `completing-read'
+(use-package flyspell-correct
+  :ensure t)
 
 (provide '+spell)
